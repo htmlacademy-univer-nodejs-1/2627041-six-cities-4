5e6d30248525895871f90610c70b115598a3442d
VartoS <ngaseneger@gmail.com>
2025-03-10 00:00:48 +0500

    Finished

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-import { CLIApplication, HelpCommand, VersionCommand } from "./cli/index.js";
+import { CLIApplication, HelpCommand, ImportCommand, VersionCommand } from "./cli/index.js";
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
   cliApplication.registerCommands([
     new HelpCommand(),
-    new VersionCommand()
+    new VersionCommand(),
+    new ImportCommand()
   ]);
 
   cliApplication.processCommand(process.argv);
