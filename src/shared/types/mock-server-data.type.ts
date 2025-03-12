0a7bc968745a5b2f0a7f5484aeb3df77bd65e3e8
n.aleksandrov <n.aleksandrov@skbkontur.ru>
2025-03-12 22:34:29 +0500

    данные и интерфейс

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,11 @@
+export type MockServerData = {
+    titles: string[];
+    descriptions: string[];
+    photoLinks: string[];
+    previewImages: string[];
+    goods: string[];
+    authorNames: string[];
+    authorEmails: string[];
+    authorAvatars: string[];
+    authorPasswords: string[];
+  };
