997aaa778a29fe84241b3075fe5e83d3152f6935
n.aleksandrov <n.aleksandrov@skbkontur.ru>
2025-03-25 20:04:35 +0500

    Добавил конфигурацию

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,11 +1,27 @@
+import { Logger } from '../logger/index.js';
+import { config } from 'dotenv';
 import { Config } from './config.interface.js';
+import { RestSchema, configRestSchema } from './index.js';
 
-export class RestConfig implements Config {
+export class RestConfig implements Config<RestSchema> {
+  private readonly config: RestSchema;
+
   constructor(
-    private readonly config: NodeJS.ProcessEnv
-  ) {}
+    private readonly logger: Logger
+    ) {
+      const parsedOutput = config();
+  
+      if (parsedOutput.error) {
+        throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+      }
+      configRestSchema.load({});
+      configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
 
-  public get(key: string): string | undefined {
+    this.config = configRestSchema.getProperties();
+      this.logger.info('.env file found and successfully parsed!');
+    }
+
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
     return this.config[key];
   }
 }
