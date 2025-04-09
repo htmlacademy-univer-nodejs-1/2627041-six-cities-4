0e8160785d2ce2592067e8f5b5c1bb68824e5728
VartoS <ngaseneger@gmail.com>
2025-04-10 00:41:57 +0500

    Добавил модельку пользователя и офера

---
+++ src/shared/modules/offer/offer.entity.ts
@@ -0,0 +1,58 @@
+import { defaultClasses, prop } from "@typegoose/typegoose";
+import { Types } from "mongoose";
+import { CityType, GoodsType, Location, Offer, OfferType, User } from "../../types/index.js";
+
+export class OfferEntity extends defaultClasses.TimeStamps implements Offer, defaultClasses.Base{
+    _id: Types.ObjectId;
+    id: string;
+    @prop()
+    title: string;
+
+    @prop()
+    description: string;
+
+    @prop()
+    date: Date;
+
+    @prop()
+    city: CityType;
+
+    @prop()
+    photoLinks: string[];
+
+    @prop()
+    previewLink: string;
+
+    @prop()
+    isPremium: boolean;
+    
+    @prop()
+    isFavorite: boolean;
+
+    @prop()
+    rate: number;
+
+    @prop()
+    type: OfferType;
+
+    @prop()
+    goods: GoodsType[];
+
+    @prop()
+    roomsCount: number;
+
+    @prop()
+    personCount: number;
+
+    @prop()
+    rentCost: number;
+
+    @prop()
+    author: User;
+
+    @prop()
+    commentsCount: number;
+    
+    @prop()
+    location: Location;
+}
