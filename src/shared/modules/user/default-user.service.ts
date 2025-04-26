b34067ae7b43e7cf56ef7306540bad381c969242
VartoS <ngaseneger@gmail.com>
2025-04-26 23:05:29 +0500

    DI и больше обвязок для пользователя

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,22 +1,51 @@
-import { UserService } from './user-service.interface.js';
-import { DocumentType } from '@typegoose/typegoose';
-import { UserEntity, UserModel } from './user.entity.js';
-import { CreateUserDto } from './dto/create-user.dto.js';
+import { UserService } from "./user-service.interface.js";
+import { DocumentType, types } from "@typegoose/typegoose";
+import { UserEntity } from "./user.entity.js";
+import { CreateUserDto } from "./dto/create-user.dto.js";
+import { inject, injectable } from "inversify";
+import { Component } from "../../types/component.enum.js";
+import { Logger } from "../../libs/logger/index.js";
 
+@injectable()
 export class DefaultUserService implements UserService {
+  constructor(
+    @inject(Component.Logger)
+    private readonly logger: Logger,
+
+    @inject(Component.UserModel)
+    private readonly userModel: types.ModelType<UserEntity>
+  ) {}
+
   findById(id: string): Promise<DocumentType<UserEntity> | null> {
-      throw new Error('Method not implemented.');
+    return this.userModel.findOne({ id });
   }
-  findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
-      throw new Error('Method not implemented.');
+  public async findByEmail(
+    email: string
+  ): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({ email });
   }
-  findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
-      throw new Error('Method not implemented.');
+  public async findOrCreate(
+    dto: CreateUserDto,
+    salt: string
+  ): Promise<DocumentType<UserEntity>> {
+    const existedUser = await this.findByEmail(dto.email);
+
+    if (existedUser) {
+      return existedUser;
+    }
+
+    return this.create(dto, salt);
   }
-  public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
+  public async create(
+    dto: CreateUserDto,
+    salt: string
+  ): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    return UserModel.create(user);
+    const result = await this.userModel.create(user);
+    this.logger.info(`New user created: ${user.email}`);
+
+    return result;
   }
 }
