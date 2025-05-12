c28eec438133049c97641763e7ce85252a3d4daf
VartoS <ngaseneger@gmail.com>
2025-05-12 23:41:14 +0500

    Добавил для комментариев сервисы

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -7,4 +7,6 @@ export const Component = {
   UserModel: Symbol.for('UserModel'),
   OfferService: Symbol.for('OfferService'),
   OfferModel: Symbol.for('OfferModel'),
+  CommentServiceInterface: Symbol.for('CommentServiceInterface'),
+  CommentModel: Symbol.for('CommentModel'),
 } as const;
