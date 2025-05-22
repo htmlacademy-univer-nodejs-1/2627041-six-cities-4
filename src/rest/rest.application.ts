056bda1348cfb5e62d54c19d13b043c12dbb1b87
VartoS <ngaseneger@gmail.com>
2025-05-22 17:02:29 +0500

    Добавил все пакеты и сделал первый контроллер

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'inversify';
+import { Express } from 'express';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Component } from '../shared/types/component.enum.js';
@@ -7,6 +8,8 @@ import { getMongoURI } from '../shared/helpers/index.js';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
@@ -25,6 +28,11 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('[Init] Application initialization');
     this.logger.info(
@@ -36,5 +44,9 @@ export class RestApplication {
     this.logger.info('[Init] Init database…');
     await this._initDb();
     this.logger.info('[Init] Init database completed');
+
+    this.logger.info('Try to init server…');
+    await this._initServer();
+    this.logger.info(`🚀 Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
